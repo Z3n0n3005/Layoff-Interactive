ad31b8cc840b4ca6adaf1f933df02ccc9e1f184f
Truc Chau <ititiu20327@student.hcmiu.edu.vn>
2023-05-17 22:16:01 +0700

    add import data file

--- api/index.js
+++ api/index.js
@@ -7,11 +7,54 @@ const JM = require('json-mapper');
 const fs = require('fs');
 
 
+const multer  = require('multer')
+// const upload = multer({ dest: '../data/' })
+const path = require('path')
+
+
 app.use(bodyParser.json({limit: '5000mb'}))
 app.use(bodyParser.urlencoded({ limit: '5000mb', extended: true }));
 app.use(express.urlencoded());
 app.use(express.json());
 app.use(cors());
+
+
+// store data file
+// fieldname = field name specified in the form (industry/company)
+// post method = "/uploadFiles"
+let storage = multer.diskStorage({
+    destination: function (req, file, cb) {
+      cb(null, '../data/')
+    },
+    filename: function (req, file, cb) {
+      cb(null, file.fieldname + ".json") //Appending extension
+    }
+  })
+  
+const upload = multer({ storage: storage });
+
+const filesUpload = () => {
+    if (req.files.industry) {
+        fs.rename("../data/industry.json", "../data/industry_" + Date.now() + ".json", function(err) {
+            if ( err ) console.log('ERROR: ' + err);
+        });
+    }
+
+    if (req.files.company) {
+        fs.rename("../data/company.json", "../data/company_" + Date.now() + ".json", function(err) {
+            if ( err ) console.log('ERROR: ' + err);
+        });
+    }
+
+    upload.fields([{ name: 'industry', maxCount: 1 }, { name: 'company', maxCount: 1 }])
+}
+app.post('/uploadFiles', filesUpload, function (req, res, next) {
+    for (const file of req.files) {
+        console.log(file)
+    }
+})
+
+
 
 const generateData = () => {
     return Array(10).fill(0).map(() => ([
